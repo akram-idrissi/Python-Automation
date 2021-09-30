724b29f5f7090d18e0e8c2f1bd515f77f567e1eb
Akram_Mo <akram.mo.gb@gmail.com>
2021-09-30 16:41:26 +0200

    Cleaning uo code

--- youtube_downloader/playlist.py
+++ youtube_downloader/playlist.py
@@ -1,50 +1,63 @@
-import os
+from moviepy.editor import *
 from pytube import Playlist
 
 
-def is_youtube_url(link):
-    """ tells if the url is a youtube url """
-    if "youtube" in link:
-        return link
-    return None
+class LoadPlaylist:
+
+    def __init__(self, link):
+        self.link = link
+
+    def is_valid(self):
+        play_list = Playlist(self.link)
+        return True if "youtube" in self.link and play_list.playlist_id else False
+
+    @staticmethod
+    def make_dir(path):
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    @staticmethod
+    def to_mp3(path_mp3, vid):
+        mp3 = "".join([os.path.splitext(vid)[0], ".mp3"])
+        mp3_path = os.path.join(path_mp3, mp3)
+
+        video = VideoFileClip(vid)
+        audio = video.audio
+        audio.write_audiofile(mp3_path)
+        audio.close()
+        video.close()
+
+    @staticmethod
+    def download(play_list):
+        for link in play_list.videos:
+            link.streams.get_highest_resolution().download()
+
+    @staticmethod
+    def load():
+        return [file_name for file_name in os.listdir() if os.path.splitext(file_name)[1] == ".mp4"]
+
+    @staticmethod
+    def clean():
+        for video in os.listdir():
+            if os.path.splitext(video)[1] == ".mp4":
+                os.remove(video)
+
+    def run(self):
+        if self.is_valid():
+            play_list = Playlist(self.link)
+            print(f"Downloading {play_list.title}..")
+            self.download(play_list)
+            mp4_videos = self.load()
+            # print(mp4_videos)
+            title = play_list.title + " - mp3"
+            self.make_dir(title)
+            for video in mp4_videos:
+                self.to_mp3(title, video)
+            self.clean()
+            print("Done")
 
 
-def new_dir(title):
-    """ makes a new directory and changes the current one """
-    if not os.path.exists(title):
-        os.makedirs(title)
-    os.chdir(title)
-
-
-def is_playlist(link):
-    """ returns url if its a playlist url """
-    try:
-        playlist = Playlist(link)
-        playlist.playlist_id
-        return playlist
-    except KeyError:
-        return None
-
-
-def playlist_download(link, audio=False, title=None):
-    ytb_url = is_youtube_url(link)
-    playlist_urls = is_playlist(link)
-
-    if playlist_urls:
-        if audio:
-            new_dir(title)
-            print(f"Start downloading {playlist_urls.title} audio format")
-            for link in playlist_urls.videos:
-                link.streams.get_audio_only().download()
-            print(f"It's done, you'll find the songs in the {title} folder")
-        else:
-            new_dir(title)
-            print(f"Start downloading {playlist_urls.title} video format")
-            for link in playlist_urls.videos:
-                link.streams.get_audio_only().download()
-            print(f"It's done, you'll find the songs in the {title} folder")
-
-
-url = "https://www.youtube.com/watch?v=j9FV40_1Z9M&list=PLIwluRGeyvBJUKr3lZJA7bskD5__FglLe"
-playlist_download(url, audio=True, title="Que La Famille")
-
+if __name__ == "__main__":
+    url = "https://www.youtube.com/watch?v=j9FV40_1Z9M&list=PLIwluRGeyvBJUKr3lZJA7bskD5__FglLe"
+    app = LoadPlaylist(url)
+    app.run()
